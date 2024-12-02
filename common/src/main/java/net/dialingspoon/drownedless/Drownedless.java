717954e2717ecdab8e2a14d7ee3ed5562894a4d8
19Phin <19phin@gmail.com>
2024-12-02 13:06:54 -0700

    Initial Commit

--- common/src/main/java/net/dialingspoon/drownedless/Drownedless.java
+++ common/src/main/java/net/dialingspoon/drownedless/Drownedless.java
@@ -2,8 +2,4 @@ package net.dialingspoon.drownedless;
 
 public final class Drownedless {
     public static final String MOD_ID = "drownedless";
-
-    public static void init() {
-        // Write common init code here.
-    }
 }
--- forge/src/main/java/net/dialingspoon/drownedless/forge/DrownedlessForge.java
+++ forge/src/main/java/net/dialingspoon/drownedless/forge/DrownedlessForge.java
@@ -1,14 +1,8 @@
 package net.dialingspoon.drownedless.forge;
 
-import dev.architectury.platform.forge.EventBuses;
 import net.dialingspoon.drownedless.Drownedless;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(Drownedless.MOD_ID)
 public final class DrownedlessForge {
-    public DrownedlessForge() {
-        // Run our common setup.
-        Drownedless.init();
-    }
 }
